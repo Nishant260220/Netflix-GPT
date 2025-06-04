78c16d7e93e51337e68c438cb3094271e399576a
Nishant Tyagi <tyaginishu015@gmail.com>
2025-06-04 22:36:01 +0530

    feat(browse):add MainContainer component & SecondaryContainer component

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,25 +1,27 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./Header";
-import { API_Constants } from "../utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryComtainer from "./SecondaryComtainer";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_Constants
-    );
-    const json = await data.json();
 
-    console.log(json.results);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
-
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer/>
+      <SecondaryComtainer/>
+      {/*
+        MainContainer
+          - VideoBackground
+          - Video Title
+
+        Secondary Container
+          - MovieList * n
+          - cards * n
+      
+      */}
     </div>
   );
 };
